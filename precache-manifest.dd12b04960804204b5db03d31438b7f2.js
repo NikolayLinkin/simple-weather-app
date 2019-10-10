338d718658edff9eea744ea9241ead40ad6c15ce
Nikolas Linkin <kolera941@gmail.com>
2019-10-10 13:04:24 +0300

    Updates

---
+++ precache-manifest.dd12b04960804204b5db03d31438b7f2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00bf0008abcbdde31866cedc8b5fe33f",
+    "url": "/test-app-algoritmika/index.html"
+  },
+  {
+    "revision": "175ada6df1ca18da1c9c",
+    "url": "/test-app-algoritmika/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "71d37fc58d3a029fd6ec",
+    "url": "/test-app-algoritmika/static/css/main.6d2a66a0.chunk.css"
+  },
+  {
+    "revision": "175ada6df1ca18da1c9c",
+    "url": "/test-app-algoritmika/static/js/2.3561edb7.chunk.js"
+  },
+  {
+    "revision": "71d37fc58d3a029fd6ec",
+    "url": "/test-app-algoritmika/static/js/main.deff5ea4.chunk.js"
+  },
+  {
+    "revision": "525b7a7d8dc5f5c9d997",
+    "url": "/test-app-algoritmika/static/js/runtime-main.32edd565.js"
+  }
+]);
